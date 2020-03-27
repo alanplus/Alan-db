cf52d75032d433967d6e641c5595ef84c456d0a9
Alan <tonyyxm@qq.com>
2020-03-27 10:05:39 +0800

    添加创建表

--- db/src/main/java/com/alan/db/base/BasePatcher.java
+++ db/src/main/java/com/alan/db/base/BasePatcher.java
@@ -10,10 +10,27 @@ import net.sqlcipher.database.SQLiteDatabase;
  * 时 间：2019-11-27
  * 简 述：<功能简述>
  */
-public abstract class BasePatcher implements IPatcher {
+public abstract class BasePatcher<T extends DbModel> implements IPatcher {
 
 
-    public <T extends DbModel> void execute(SQLiteDatabase database, Class<T> tClass, String[] columns) {
+    /**
+     * 添加列
+     *
+     * @param database
+     * @param tClass
+     * @param columns
+     */
+    public void update(SQLiteDatabase database, Class<T> tClass, String[] columns) {
         PatcherHelper.alertTableForAdd(columns, database, tClass);
+    }
+
+    /**
+     * 创建表
+     * @param database
+     * @param tClass
+     */
+    public void create(SQLiteDatabase database, Class<T> tClass) {
+        PatcherHelper.createTable(tClass, database);
+
     }
 }
